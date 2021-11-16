b26948623871d029e904518487b5b70b2077db35
dometa <dometa@msn.com>
2021-11-16 11:35:43 +0700

    เพิ่มไฟล์ blure_image.m และ เพิ่ม comment ใน softfocus_new.m

--- softfocus_new.m
+++ softfocus_new.m
@@ -1,3 +1,4 @@
+% นำเข้าข้อมูลภาพ
 im = im2double(imread('portrait.jpg'));
 
 %Step1: Duplicate
@@ -8,7 +9,11 @@ im2 = im;
 imblur = imgaussfilt(im2,13.5);
 %figure,imshow([imblur,imblur6])
 %clear im2;
+
 %Step 3: Change Opacity of Blurred Image (0-100) via imblend.m (imblend.m needs imcast.m to run properly)
 output = imblend(imblur, im, 0.5, 'normal');
 
+output = im2uint8(output);
+
+% แสดงผลภาพ
 imshow(output)
